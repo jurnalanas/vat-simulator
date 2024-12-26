08359bbb44315d670a428dc817a198aaedd2b771
Muhammad Nasrurrohman <anas@jurnalanas.com>
2024-12-26 18:01:31 +0700

    Update footer link

--- src/components/vat/footer.tsx
+++ src/components/vat/footer.tsx
@@ -5,7 +5,11 @@ export function Footer() {
     <div className="mt-8 rounded-sm border border-slate-200 bg-gradient-to-br from-slate-50 to-slate-100 p-4 text-center text-slate-900 lg:mt-12">
       <p className="text-center">
         Mau belajar programming dan mulai coding career? Cek panduannya{" "}
-        <Link href="/#waitlist" className="text-blue-800">
+        <Link
+          href="https://panduancod.ing/#waitlist"
+          className="text-blue-800"
+          target="_blank"
+        >
           di sini
         </Link>
         <span className="ml-2 text-sm">🚀</span>
